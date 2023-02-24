3f35d70fd2e15977906319db73660d9f1fe8bc80
Mateusz Piotrowski <0mp@FreeBSD.org>
2023-02-24 10:28:34 +0100

    Add a list of available formulae to README

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# 0mp Essentials
+# 0mp's Homebrew Tap of Essentials
+
+## Available formulae
+
+| Formula | Description |
+| --- | --- |
+| goat |  POSIX-compliant shell movement boosting hack for real ninjas |
+| hyperfine-scripts |  Scripts for data analysis of Hyperfine benchmarking results |
+| mantra |  CLI for live preview of mandoc manual pages for documentation contributors |
+| matplotlib |  Python 2D plotting library |
+| passh |  Non-interactive password inputting CLI |
 
 ## How do I install these formulae?
 
---
+++ README.md.sh
@@ -0,0 +1,31 @@
+#! /bin/sh -
+
+tab_character='	'
+
+set -eu
+
+cat << 'EOF'
+# 0mp's Homebrew Tap of Essentials
+
+## Available formulae
+
+EOF
+
+{
+	printf '%s\t%s\n' "Formula" "Description"
+	brew desc --eval-all --formula Formula/* |  sed "s/:/$tab_character/"
+} | mlr --itsv --omd cat
+
+echo
+
+cat << 'EOF'
+## How do I install these formulae?
+
+`brew install 0mp/essentials/<formula>`
+
+Or `brew tap 0mp/essentials` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+EOF
