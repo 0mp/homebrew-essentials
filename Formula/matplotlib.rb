2f317c9d8b39e5d2f3a6425ab120231e83aa4414
Mateusz Piotrowski <0mp@FreeBSD.org>
2023-02-24 10:47:36 +0100

    matplotlib: Fix a typo

--- Formula/matplotlib.rb
+++ Formula/matplotlib.rb
@@ -38,7 +38,7 @@ class Matplotlib < Formula
   depends_on "six"
 
   # Regenerate resource blocks with:
-  # brew update-python-resources --exclude-packages fonttools,numpy,pillow,six matplotlib^C
+  # brew update-python-resources --exclude-packages fonttools,numpy,pillow,six matplotlib
 
   resource "contourpy" do
     url "https://files.pythonhosted.org/packages/b4/9b/6edb9d3e334a70a212f66a844188fcb57ddbd528cbc3b1fe7abfc317ddd7/contourpy-1.0.7.tar.gz"
