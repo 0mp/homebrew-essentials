5777a86ee623ab6e11eca96753282e22ed866c9b
Mateusz Piotrowski <0mp@FreeBSD.org>
2024-07-15 19:11:08 +0200

    git-arc: Refactor URL handling

--- Formula/git-arc.rb
+++ Formula/git-arc.rb
@@ -3,10 +3,11 @@
 class GitArc < Formula
   url_prefix = "https://cgit.freebsd.org/src/plain/tools/tools/git/"
   rev = "76a8625bc71da3fa63eef5abb4782553d2588f5c"
+  url_suffix = "?id=" + rev
 
   desc "Wrapper to improve integration between git and arcanist"
   homepage "https://cgit.freebsd.org/src/plain/tools/tools/git/"
-  url url_prefix + "/git-arc.sh?id=" + rev
+  url url_prefix + "git-arc.sh" + url_suffix
   license "BSD-2-Clause"
   sha256 "2125d0a57c522ab24a43091f851dbdd9b33633699eef84195c480630301335da"
   version "20240422" # Follows FreeBSD's devel/freebsd-git-devtools.
@@ -16,7 +17,7 @@ class GitArc < Formula
   depends_on "jq"
 
   resource "manpage" do
-    url "https://cgit.freebsd.org/src/plain/tools/tools/git/git-arc.1?id=76a8625bc71da3fa63eef5abb4782553d2588f5c"
+    url url_prefix + "git-arc.1" + url_suffix
     sha256 "3b7660960d39bbe310194f985d5cbc08836cad249e29da0692dfe7945275dc8e"
   end
 
